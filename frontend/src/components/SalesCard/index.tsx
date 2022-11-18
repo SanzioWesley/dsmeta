174bc69d5bac7e45e70aaca90b1d98364a6dd78a
Sanzio Wesley <sanziowesley@hotmail.com>
2022-11-18 20:13:33 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -24,9 +24,7 @@ function SalesCard() {
         const dmin = minDate.toISOString().slice(0, 10);
         const dmax = maxDate.toISOString().slice(0, 10);
 
-        console.log(dmin);
-        console.log(dmax);
-        
+
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
@@ -71,25 +69,26 @@ function SalesCard() {
                     </thead>
                     <tbody>
                         {
-                             sales.map(sale => {
+                            sales.map(sale => {
                                 return (
                                     <tr key={sale.id}>
-                                    <td className="show992">{sale.id}</td>
-                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>                                             
-                                    <td>{sale.sellerName}</td>
-                                    <td className="show992">{sale.visited}</td>
-                                    <td className="show992">11{sale.deals}</td>
-                                    <td>{sale.amount.toFixed(2)}</td>
-                                    <td>
-                                        <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
-                                        </div>
-                                    </td>
-                                </tr>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>R$ {sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="dsmeta-red-btn-container">
+                                                <NotificationButton saleId={sale.id} />
+                                            </div>
+                                        </td>
+                                    </tr>
                                 )
-                            })}
-                        
-                       
+                            })
+                        }
+
+
                     </tbody>
 
                 </table>
